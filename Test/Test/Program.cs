b3ee92e161b12ed68ceb69d86ae2a864d8b65aa6
yukit1028 <rosemary441039@gmail.com>
2016-02-18 06:21:45 +0900

    a
    a

---
+++ Test/Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
