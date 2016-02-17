4eb7e76c064a8f9417f1b896999b15935cf45a18
yukit1028 <rosemary441039@gmail.com>
2016-02-18 07:16:07 +0900

    インスタンスgunの追加
    インスタンスgunの追加及び実装

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -4,22 +4,27 @@ namespace Test
 {
 	class Program
 	{
-		public static void Main(string[] args)
+		public static void Main()
 		{
 			Weapon sword = new Weapon();
 			sword.damage = 100;
 			sword.magic = 20;
 			
-			int damage = sword.damageCalculate();
+			Weapon gun = new Weapon();
+			gun.damage = 150;
+			gun.magic = 0;
 			
-			Console.WriteLine(damage);
+			int sword_damage = sword.damageCalculate();
+			int gun_damage = gun.damageCalculate();
+			
+			Console.WriteLine(sword_damage);
+			Console.WriteLine(gun_damage);
 			Console.ReadKey(true);
 		}
 	}
 	class Weapon
 	{
-		public int damage;
-		public int magic;
+		public int damage,magic;
 		
 		public int damageCalculate()
 		{
