3f035a02e7510b0e7e0fbe2d2f09d4fae5af411a
danganed <g156705@ex.osaka-kyoiku.ac.jp>
2016-02-18 07:20:10 +0900

    あ
    ｂ

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -14,11 +14,17 @@ namespace Test
 			gun.damage = 150;
 			gun.magic = 0;
 			
+			Weapon stuff = new Weapon();
+			stuff.damage = 20;
+			stuff.magic = 85;
+			
 			int sword_damage = sword.damageCalculate();
 			int gun_damage = gun.damageCalculate();
+			int stuff_damage = stuff.damageCalculate();
 			
 			Console.WriteLine(sword_damage);
 			Console.WriteLine(gun_damage);
+			Console.WriteLine(stuff_damage);
 			Console.ReadKey(true);
 		}
 	}
